db8feadcbd63fd951d5321ac41c043074090585f
vitko5 <cp.advert0@googlemail.com>
2022-07-22 16:50:54 +0300

    Push

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private final String name;
+    private final String id;
+    // transient
+    private final Salary salary;
+
+    public Employee(String name, String id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = new Salary(salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary.getSalary();
+    }
 }
--- src/Salary.java
+++ src/Salary.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Salary {
+import java.io.Serializable;
+
+public class Salary implements Serializable {
+    private final transient int salary;
+
+    public Salary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
